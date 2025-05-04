aec738840c5a3960348fadba7a75a789d83d76d8
Nikita Konovalov <nkonoval@redhat.com>
2025-05-04 19:51:36 +0200

    created annotation api

---
+++ src/main/java/AnntotationAPI.java
@@ -0,0 +1,17 @@
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+public class AnntotationAPI {
+
+    public class Annotations {
+
+        @Retention(RetentionPolicy.RUNTIME)
+        @Target(ElementType.METHOD)
+        public @interface AiSnippet {
+            String valueQuestion(); // question about the code
+            String valueAnswer(); //answer about the code
+        }
+    }
+}
