0c8c8b66e41740855a50aa4b935c5ce2fcae4985
Nikita Konovalov <nkonoval@redhat.com>
2025-05-12 08:19:54 +0200

    changed target scope

--- src/main/java/org/aiannotation/AiSnippet.java
+++ src/main/java/org/aiannotation/AiSnippet.java
@@ -6,9 +6,9 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
+@Target({ElementType.METHOD, ElementType.FIELD})
 public @interface AiSnippet {
     String question(); // question about the code
-    String answer(); //answer about the code
+    String answer(); // answer about the code
 }
 
